b2509ba7836d0ac4c1277d2cedb98b92f3580a9a
Ebates10 <emilyspbates@gmail.com>
2023-09-22 06:57:25 -0400

    refactored code pertaining to container number info into Container class

--- imdl-check-digit-calculator/Classes/Container.cs
+++ imdl-check-digit-calculator/Classes/Container.cs
@@ -8,6 +8,188 @@ namespace imdl_check_digit_calculator.Classes
 {
     public class Container
     {
+        public string ContainerNumber { get; set; }
 
+        public string ContainerPreFix { get { return ContainerNumber.Substring(0, 3); } }
+        public string EqCategoryIdentifier { get { return ContainerNumber.Substring(3, 1); } }
+        public string ContainerSerialNumber { get { return ContainerNumber.Substring(4, 6); } }
+
+        public Container(string containerNumber)
+        {
+            ContainerNumber = containerNumber;
+        }
+
+
+        public int GetCheckDigit(string containerNumber)
+        {
+            const int valueA = 10;
+            const int valueB = 12;
+            const int valueC = 13;
+            const int valueD = 14;
+            const int valueE = 15;
+            const int valueF = 16;
+            const int valueG = 17;
+            const int valueH = 18;
+            const int valueI = 19;
+            const int valueJ = 20;
+            const int valueK = 21;
+            const int valueL = 23;
+            const int valueM = 24;
+            const int valueN = 25;
+            const int valueO = 26;
+            const int valueP = 27;
+            const int valueQ = 28;
+            const int valueR = 29;
+            const int valueS = 30;
+            const int valueT = 31;
+            const int valueU = 32;
+            const int valueV = 34;
+            const int valueW = 35;
+            const int valueX = 36;
+            const int valueY = 37;
+            const int valueZ = 38;
+
+            int[] multipliers = new int[containerNumber.Length];
+            for (int i = 0; i < containerNumber.Length; i++)
+            {
+                int multiplier = Convert.ToInt32(Math.Pow(2, i));
+                multipliers[i] = multiplier;
+            }
+
+            int[] values = new int[containerNumber.Length];
+            for (int i = 0; i < containerNumber.Length; i++)
+            {
+                if (containerNumber[i] == 'A')
+                {
+                    values[i] = valueA;
+                }
+                else if (containerNumber[i] == 'B')
+                {
+                    values[i] = valueB;
+                }
+                else if (containerNumber[i] == 'C')
+                {
+                    values[i] = valueC;
+                }
+                else if (containerNumber[i] == 'D')
+                {
+                    values[i] = valueD;
+                }
+                else if (containerNumber[i] == 'E')
+                {
+                    values[i] = valueE;
+                }
+                else if (containerNumber[i] == 'F')
+                {
+                    values[i] = valueF;
+                }
+                else if (containerNumber[i] == 'G')
+                {
+                    values[i] = valueG;
+                }
+                else if (containerNumber[i] == 'H')
+                {
+                    values[i] = valueH;
+                }
+                else if (containerNumber[i] == 'I')
+                {
+                    values[i] = valueI;
+                }
+                else if (containerNumber[i] == 'J')
+                {
+                    values[i] = valueJ;
+                }
+                else if (containerNumber[i] == 'K')
+                {
+                    values[i] = valueK;
+                }
+                else if (containerNumber[i] == 'L')
+                {
+                    values[i] = valueL;
+                }
+                else if (containerNumber[i] == 'M')
+                {
+                    values[i] = valueM;
+                }
+                else if (containerNumber[i] == 'N')
+                {
+                    values[i] = valueN;
+                }
+                else if (containerNumber[i] == 'O')
+                {
+                    values[i] = valueO;
+                }
+                else if (containerNumber[i] == 'P')
+                {
+                    values[i] = valueP;
+                }
+                else if (containerNumber[i] == 'Q')
+                {
+                    values[i] = valueQ;
+                }
+                else if (containerNumber[i] == 'R')
+                {
+                    values[i] = valueR;
+                }
+                else if (containerNumber[i] == 'S')
+                {
+                    values[i] = valueS;
+                }
+                else if (containerNumber[i] == 'T')
+                {
+                    values[i] = valueT;
+                }
+                else if (containerNumber[i] == 'U')
+                {
+                    values[i] = valueU;
+                }
+                else if (containerNumber[i] == 'V')
+                {
+                    values[i] = valueV;
+                }
+                else if (containerNumber[i] == 'W')
+                {
+                    values[i] = valueW;
+                }
+                else if (containerNumber[i] == 'X')
+                {
+                    values[i] = valueX;
+                }
+                else if (containerNumber[i] == 'Y')
+                {
+                    values[i] = valueY;
+                }
+                else if (containerNumber[i] == 'Z')
+                {
+                    values[i] = valueZ;
+                }
+                else
+                {
+                    values[i] = Int32.Parse(containerNumber.Substring(i, 1));
+                }
+            }
+
+            int sumOfValuesAfterMultiplier = 0;
+            for (int i = 0; i < containerNumber.Length; i++)
+            {
+                int number = values[i] * multipliers[i];
+                sumOfValuesAfterMultiplier += number;
+            }
+
+            int sumDividedByEleven = sumOfValuesAfterMultiplier / 11;
+            int sumDivByElevenAndMultByEleven = sumDividedByEleven * 11;
+            int checkDigit;
+
+            if (sumOfValuesAfterMultiplier - sumDivByElevenAndMultByEleven == 10)
+            {
+                checkDigit = 0;
+            }
+            else
+            {
+                checkDigit = sumOfValuesAfterMultiplier - sumDivByElevenAndMultByEleven;
+            }
+
+            return checkDigit;
+        }
     }
 }
--- imdl-check-digit-calculator/Classes/UserInterface.cs
+++ imdl-check-digit-calculator/Classes/UserInterface.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Formats.Asn1.AsnWriter;
+using imdl_check_digit_calculator.Classes;
 
 namespace imdl_check_digit_calculator.Classes
 {
@@ -18,25 +19,25 @@ namespace imdl_check_digit_calculator.Classes
             {
                 string containerNumber = GetContainerNumber();
 
-                string containerPreFix = containerNumber.Substring(0, 3);
-                string eqCategoryIdentifier = containerNumber.Substring(3, 1);
-                string containerSerialNumber = containerNumber.Substring(4, 6);
+                Container container = new Container(containerNumber); 
 
-                DisplayContNumberComponentInfo(containerPreFix, eqCategoryIdentifier, containerSerialNumber);
+                DisplayContNumberComponentInfo(container.ContainerPreFix, container.EqCategoryIdentifier, container.ContainerSerialNumber);
 
-                GetCheckDigit(containerNumber);
-                isDone = false;
-                Console.ForegroundColor = ConsoleColor.White;
+                int checkDigit = container.GetCheckDigit(container.ContainerNumber);
+
+                DisplayCheckDigit(checkDigit);
+
+                isDone = true;
             }
 
 
         }
 
-        private static string GetContainerNumber()
+        public static string GetContainerNumber()
         {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("Please provide the container number in the format XXXX######: ");
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             string containerNumber = Console.ReadLine().ToUpper();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             return containerNumber;
@@ -63,193 +64,12 @@ namespace imdl_check_digit_calculator.Classes
             Console.ForegroundColor = ConsoleColor.DarkGreen;
         }
 
-
-        private static void GetCheckDigit(string containerNumber)
+        private static void DisplayCheckDigit(int checkDigit)
         {
-            const int valueA = 10;
-            const int valueB = 12;
-            const int valueC = 13;
-            const int valueD = 14;
-            const int valueE = 15;
-            const int valueF = 16;
-            const int valueG = 17;
-            const int valueH = 18;
-            const int valueI = 19;
-            const int valueJ = 20;
-            const int valueK = 21;
-            const int valueL = 23;
-            const int valueM = 24;
-            const int valueN = 25;
-            const int valueO = 26;
-            const int valueP = 27;
-            const int valueQ = 28;
-            const int valueR = 29;
-            const int valueS = 30;
-            const int valueT = 31;
-            const int valueU = 32;
-            const int valueV = 34;
-            const int valueW = 35;
-            const int valueX = 36;
-            const int valueY = 37;
-            const int valueZ = 38;
-
-            const int firstDigitMultiplier = 1;
-            const int secondDigitMultiplier = 2;
-            const int thirdDigitMultiplier = 4;
-            const int fourthDigitMultiplier = 8;
-            const int fifthDigitMultiplier = 16;
-            const int sixthDigitMultiplier = 32;
-            const int seventhDigitMultiplier = 64;
-            const int eighthDigitMultiplier = 128;
-            const int ninthDigitMultiplier = 256;
-            const int tenthDigitMultiplier = 512;
-
-            int[] multipliers = new int[containerNumber.Length];
-            for (int i = 0; i < containerNumber.Length; i++)
-            {
-                int multiplier = Convert.ToInt32(Math.Pow(2, i));
-                multipliers[i] = multiplier;
-            }
-
-            int[] values = new int[containerNumber.Length];
-            for (int i = 0; i < containerNumber.Length; i++)
-            {
-                if (containerNumber[i] == 'A')
-                {
-                    values[i] = valueA;
-                }
-                else if (containerNumber[i] == 'B')
-                {
-                    values[i] = valueB;
-                }
-                else if (containerNumber[i] == 'C')
-                {
-                    values[i] = valueC;
-                }
-                else if (containerNumber[i] == 'D')
-                {
-                    values[i] = valueD;
-                }
-                else if (containerNumber[i] == 'E')
-                {
-                    values[i] = valueE;
-                }
-                else if (containerNumber[i] == 'F')
-                {
-                    values[i] = valueF;
-                }
-                else if (containerNumber[i] == 'G')
-                {
-                    values[i] = valueG;
-                }
-                else if (containerNumber[i] == 'H')
-                {
-                    values[i] = valueH;
-                }
-                else if (containerNumber[i] == 'I')
-                {
-                    values[i] = valueI;
-                }
-                else if (containerNumber[i] == 'J')
-                {
-                    values[i] = valueJ;
-                }
-                else if (containerNumber[i] == 'K')
-                {
-                    values[i] = valueK;
-                }
-                else if (containerNumber[i] == 'L')
-                {
-                    values[i] = valueL;
-                }
-                else if (containerNumber[i] == 'M')
-                {
-                    values[i] = valueM;
-                }
-                else if (containerNumber[i] == 'N')
-                {
-                    values[i] = valueN;
-                }
-                else if (containerNumber[i] == 'O')
-                {
-                    values[i] = valueO;
-                }
-                else if (containerNumber[i] == 'P')
-                {
-                    values[i] = valueP;
-                }
-                else if (containerNumber[i] == 'Q')
-                {
-                    values[i] = valueQ;
-                }
-                else if (containerNumber[i] == 'R')
-                {
-                    values[i] = valueR;
-                }
-                else if (containerNumber[i] == 'S')
-                {
-                    values[i] = valueS;
-                }
-                else if (containerNumber[i] == 'T')
-                {
-                    values[i] = valueT;
-                }
-                else if (containerNumber[i] == 'U')
-                {
-                    values[i] = valueU;
-                }
-                else if (containerNumber[i] == 'V')
-                {
-                    values[i] = valueV;
-                }
-                else if (containerNumber[i] == 'W')
-                {
-                    values[i] = valueW;
-                }
-                else if (containerNumber[i] == 'X')
-                {
-                    values[i] = valueX;
-                }
-                else if (containerNumber[i] == 'Y')
-                {
-                    values[i] = valueY;
-                }
-                else if (containerNumber[i] == 'Z')
-                {
-                    values[i] = valueZ;
-                }
-                else
-                {
-                    values[i] = Int32.Parse(containerNumber.Substring(i, 1));
-                }
-                Console.WriteLine(values[i]);
-            }
-
-            int sumOfValuesAfterMultiplier = 0;
-            for (int i = 0; i < containerNumber.Length; i++)
-            {
-                int number = values[i] * multipliers[i];
-                sumOfValuesAfterMultiplier += number;
-                Console.WriteLine(number);
-            }
-            Console.WriteLine("Sum = " + sumOfValuesAfterMultiplier);
-
-            int sumDividedByEleven = sumOfValuesAfterMultiplier / 11;
-            int sumDivByElevenAndMultByEleven = sumDividedByEleven * 11;
-            int checkDigit;
-
-            if (sumOfValuesAfterMultiplier - sumDivByElevenAndMultByEleven == 10)
-            {
-                checkDigit = 0;
-            }
-            else
-            {
-                checkDigit = sumOfValuesAfterMultiplier - sumDivByElevenAndMultByEleven;
-            }
-
-            Console.WriteLine("Check digit = " + checkDigit);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Check digit = {checkDigit}");
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
-
 
     }
 }
