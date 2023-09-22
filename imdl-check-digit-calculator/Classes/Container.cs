59ddb2d3762629a136d3b43a4d634b2da507ef50
Ebates10 <emilyspbates@gmail.com>
2023-09-22 07:22:21 -0400

    refactored to use switch case instead of if/else statements

--- imdl-check-digit-calculator/Classes/Container.cs
+++ imdl-check-digit-calculator/Classes/Container.cs
@@ -22,33 +22,6 @@ namespace imdl_check_digit_calculator.Classes
 
         public int GetCheckDigit(string containerNumber)
         {
-            const int valueA = 10;
-            const int valueB = 12;
-            const int valueC = 13;
-            const int valueD = 14;
-            const int valueE = 15;
-            const int valueF = 16;
-            const int valueG = 17;
-            const int valueH = 18;
-            const int valueI = 19;
-            const int valueJ = 20;
-            const int valueK = 21;
-            const int valueL = 23;
-            const int valueM = 24;
-            const int valueN = 25;
-            const int valueO = 26;
-            const int valueP = 27;
-            const int valueQ = 28;
-            const int valueR = 29;
-            const int valueS = 30;
-            const int valueT = 31;
-            const int valueU = 32;
-            const int valueV = 34;
-            const int valueW = 35;
-            const int valueX = 36;
-            const int valueY = 37;
-            const int valueZ = 38;
-
             Dictionary<char, int> letterValues = new Dictionary<char, int>()
             {
                 {'A', 10 },
@@ -89,113 +62,89 @@ namespace imdl_check_digit_calculator.Classes
             int[] values = new int[containerNumber.Length];
             for (int i = 0; i < containerNumber.Length; i++)
             {
-                if (containerNumber[i] == 'A')
+                switch(containerNumber[i])
                 {
-                    values[i] = letterValues['A'];
-                }
-                else if (containerNumber[i] == 'B')
-                {
-                    values[i] = letterValues['B'];
-                }
-                else if (containerNumber[i] == 'C')
-                {
-                    values[i] = letterValues['C'];
-                }
-                else if (containerNumber[i] == 'D')
-                {
-                    values[i] = letterValues['D'];
-                }
-                else if (containerNumber[i] == 'E')
-                {
-                    values[i] = letterValues['E'];
-                }
-                else if (containerNumber[i] == 'F')
-                {
-                    values[i] = letterValues['F'];
-                }
-                else if (containerNumber[i] == 'G')
-                {
-                    values[i] = letterValues['G'];
-                }
-                else if (containerNumber[i] == 'H')
-                {
-                    values[i] = letterValues['H'];
-                }
-                else if (containerNumber[i] == 'I')
-                {
-                    values[i] = letterValues['I'];
-                }
-                else if (containerNumber[i] == 'J')
-                {
-                    values[i] = letterValues['J'];
-                }
-                else if (containerNumber[i] == 'K')
-                {
-                    values[i] = letterValues['K'];
-                }
-                else if (containerNumber[i] == 'L')
-                {
-                    values[i] = letterValues['L'];
-                }
-                else if (containerNumber[i] == 'M')
-                {
-                    values[i] = letterValues['M'];
-                }
-                else if (containerNumber[i] == 'N')
-                {
-                    values[i] = letterValues['N'];
-                }
-                else if (containerNumber[i] == 'O')
-                {
-                    values[i] = letterValues['O'];
-                }
-                else if (containerNumber[i] == 'P')
-                {
-                    values[i] = letterValues['P'];
-                }
-                else if (containerNumber[i] == 'Q')
-                {
-                    values[i] = letterValues['Q'];
-                }
-                else if (containerNumber[i] == 'R')
-                {
-                    values[i] = letterValues['R'];
-                }
-                else if (containerNumber[i] == 'S')
-                {
-                    values[i] = letterValues['S'];
-                }
-                else if (containerNumber[i] == 'T')
-                {
-                    values[i] = letterValues['T'];
-                }
-                else if (containerNumber[i] == 'U')
-                {
-                    values[i] = letterValues['U'];
-                }
-                else if (containerNumber[i] == 'V')
-                {
-                    values[i] = letterValues['V'];
-                }
-                else if (containerNumber[i] == 'W')
-                {
-                    values[i] = letterValues['W'];
-                }
-                else if (containerNumber[i] == 'X')
-                {
-                    values[i] = letterValues['X'];
-                }
-                else if (containerNumber[i] == 'Y')
-                {
-                    values[i] = letterValues['Y'];
-                }
-                else if (containerNumber[i] == 'Z')
-                {
-                    values[i] = letterValues['Z'];
-                }
-                else
-                {
-                    values[i] = Int32.Parse(containerNumber.Substring(i, 1));
+                    case 'A':
+                        values[i] = letterValues['A'];
+                        break;
+                    case 'B':
+                        values[i] = letterValues['B'];
+                        break;
+                    case 'C':
+                        values[i] = letterValues['C'];
+                        break;
+                    case 'D':
+                        values[i] = letterValues['D'];
+                        break;
+                    case 'E':
+                        values[i] = letterValues['E'];
+                        break;
+                    case 'F':
+                        values[i] = letterValues['F'];
+                        break;
+                    case 'G':
+                        values[i] = letterValues['G'];
+                        break;
+                    case 'H':
+                        values[i] = letterValues['H'];
+                        break;
+                    case 'I':
+                        values[i] = letterValues['I'];
+                        break;
+                    case 'J':
+                        values[i] = letterValues['J'];
+                        break;
+                    case 'K':
+                        values[i] = letterValues['K'];
+                        break;
+                    case 'L':
+                        values[i] = letterValues['L'];
+                        break;
+                    case 'M':
+                        values[i] = letterValues['M'];
+                        break;
+                    case 'N':
+                        values[i] = letterValues['N'];
+                        break;
+                    case 'O':
+                        values[i] = letterValues['O'];
+                        break;
+                    case 'P':
+                        values[i] = letterValues['P'];
+                        break;
+                    case 'Q':
+                        values[i] = letterValues['Q'];
+                        break;
+                    case 'R':
+                        values[i] = letterValues['R'];
+                        break;
+                    case 'S':
+                        values[i] = letterValues['S'];
+                        break;
+                    case 'T':
+                        values[i] = letterValues['T'];
+                        break;
+                    case 'U':
+                        values[i] = letterValues['U'];
+                        break;
+                    case 'V':
+                        values[i] = letterValues['V'];
+                        break;
+                    case 'W':
+                        values[i] = letterValues['W'];
+                        break;
+                    case 'X':
+                        values[i] = letterValues['X'];
+                        break;
+                    case 'Y':
+                        values[i] = letterValues['Y'];
+                        break;
+                    case 'Z':
+                        values[i] = letterValues['Z'];
+                        break;
+                    default:
+                        values[i] = Int32.Parse(containerNumber.Substring(i, 1));
+                        break;
                 }
             }
 
